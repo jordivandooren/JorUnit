afb45cf8f41b26effa5fa3e123ad63f0c7f657dd
jordi <jordivandooren@gmail.com>
2019-08-02 21:17:50 +0200

    Add setUp method to Prototype, called by testMethod.

--- TestPrototype.java
+++ TestPrototype.java
@@ -5,6 +5,7 @@ import JorUnit.Checker;
 class TestPrototype {
     public static void main(String[] args) {
         testPrototypeTestMethod();
+        testSetUp();
     }
 
     static void testPrototypeTestMethod() {
@@ -14,18 +15,33 @@ class TestPrototype {
         test.testMethod();
         check.areEqual(1, test.wasRun);
     }
+
+    static void testSetUp() {
+        Prototype test = new Prototype();
+        Checker check = new Checker();
+        check.areEqual(0, test.wasSetUp);
+        test.testMethod();
+        check.areEqual(1, test.wasSetUp);
+    }
 }
 
 
 class Prototype { 
     int wasRun;
+    int wasSetUp;
 
     Prototype() {
+        wasSetUp = 0;
         wasRun = 0;
     }
 
     public void testMethod() {
+        setUp();
         wasRun = 1;
+    }
+
+    void setUp() {
+        wasSetUp = 1;
     }
 }
         
