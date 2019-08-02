5acadfb93bec7e1f9a108566f469281e4f924639
jordi <jordivandooren@gmail.com>
2019-08-02 23:21:50 +0200

    Try to move setUp and tearDown out of testMethod using Template Method.

--- TestPrototype.java
+++ TestPrototype.java
@@ -7,32 +7,45 @@ class TestPrototype {
 
     static void testTestMethod() {
         Prototype test = new Prototype();
-        test.testMethod();
+        test.run();
         boolean result = test.log.equals("setup method teardown");
         System.out.println(result);
     }
 }
 
 
-class Prototype { 
+abstract class PrototypeTemplate {
     String log;
 
-    Prototype() {
+    PrototypeTemplate() {
         log = "";
     }
 
-    public void testMethod() {
+    public void run() {
         setUp();
-        log += " method";
+        testMethod();
         tearDown();
     }
-
+ 
     void setUp() {
         log += "setup";
     }
     
+    abstract void testMethod();
+
     void tearDown() {
         log += " teardown";
     }
 }
-        
+    
+
+class Prototype extends PrototypeTemplate { 
+    
+    Prototype() {
+        super();
+    }
+
+    void testMethod() {
+        log += " method";
+    }
+}
